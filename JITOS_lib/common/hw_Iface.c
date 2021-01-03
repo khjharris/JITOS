901fa627d7e44e78ec5beac93687f42ec3390e2c
Noah Malhi <noahmalhi.6624@gmail.com>
2021-01-02 21:24:57 -0500

    Some syntax fixes and adding a new init funtion. Would do more  but tired....

--- JITOS_lib/common/hw_Iface.c
+++ JITOS_lib/common/hw_Iface.c
@@ -1,34 +1,25 @@
 //This file contains the interfacing for the supported peripherals
 //
 //
-//
-//
 //Noah Malhi  1/2/2021
 
 //Includes
 #include  "hw_Iface.h"
 
+// Static function defines
+static void general_timer_callback();
+static void pwm_timer_callback();
+
 // Local variables
 uint8_t timeout = 0;
 
 
-static void general_timer_callback()
+/* PUBLIC FUNCTIONS *****************************************************************************************************/
+void init_general_timer(
 {
-    if( TIM->SR & 0x1 )
-    {
-        timeout = 1;
-        GEN_TIM->SR &= ~( UPDATE_INTERRUPT_FLAG );  //Clear SR register
-        GEN_TIM->CR1 |= (~((U16)TIM_CR1_EN));  //Disable the timer
-        GEN_TIM->CNT = 0x1; //Reset the timer
-    }
+    
 
 }
-
-static void pwm_timer_callback()
-{
-
-}
-
 void start_general_timer( n )
 {
     GEN_TIM->CR1 |= TIM_CR1_EN;  //Enable the timer
@@ -47,4 +38,22 @@ void read_general_timer()
 void stop_general_timer() 
 {
   GEN_TIM->CR1 |= (~((U16) TIM_CR1_EN));
+}
+
+/*PRIVATE FUNCTIONS ************************************************************************************************************/
+static void general_timer_callback()
+{
+    if( TIM->SR & 0x1 )
+    {
+        timeout = 1;
+        GEN_TIM->SR &= ~( UPDATE_INTERRUPT_FLAG );  //Clear SR register
+        GEN_TIM->CR1 |= (~((U16)TIM_CR1_EN));  //Disable the timer
+        GEN_TIM->CNT = 0x1; //Reset the timer
+    }
+
+}
+
+static void pwm_timer_callback()
+{
+
 }
--- JITOS_lib/common/hw_Iface.h
+++ JITOS_lib/common/hw_Iface.h
@@ -6,11 +6,12 @@
 #define TIM_CR1_EN              0x1UL
 #define UPDATE_INTERRUPT_FLAG   0x1
 
+void init_general_timer();
 void start_pwm_timer();
-
 void start_general_timer();
-void read_general_timer(
-void stop_general_timer() 
+void read_general_timer();
+void stop_general_timer();
+
 #endif
 
 
